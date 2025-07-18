7a09b9b59f39a3ec9114047f5dfac5fcb7666e9e
hannamatheos <hannamatheos@gmail.com>
2025-07-18 23:03:47 +0800

    add all files to github

---
+++ terraform/main.tf
@@ -0,0 +1,81 @@
+locals {
+  prefix = "hanna-dev"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_vpc" "hanna_vpc" {
+  tags = {
+    Name = var.vpc_name
+  }
+
+}
+
+data "aws_subnets" "hanna_public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.hanna_vpc.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"] # Matches names like "hanna-vpc-public-ap-southeast-1a"
+  }
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name = "${local.prefix}-ecr"
+  force_delete =  true
+}
+
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.9.0"
+
+  cluster_name = "${local.prefix}-ecs"
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+      }
+    }
+  }
+
+  services = {
+    HANNA-TASKDEFINITION = { #task definition and service name -> #Change
+      cpu    = 512
+      memory = 1024
+      container_definitions = {
+        HANNA-ECS-CONTAINER = { #container name -> Change
+          essential = true
+          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.id}.amazonaws.com/${local.prefix}-ecr:latest"
+          port_mappings = [
+            {
+              containerPort = 8080
+              protocol      = "tcp"
+            }
+          ]
+        }
+      }
+      assign_public_ip                   = true
+      deployment_minimum_healthy_percent = 100
+      subnet_ids                   = flatten(data.aws_subnets.hanna_public.ids) #List of subnet IDs to use for your tasks
+      security_group_ids           = [module.ecs_sg.security_group_id] #Create a SG resource and pass it here
+    }
+  }
+}
+
+module "ecs_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.1.0"
+
+  name = "${local.prefix}-ecs-sg"
+  description = "Security group for ecs"
+  vpc_id = data.aws_vpc.hanna_vpc.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["http-8080-tcp"]
+  egress_rules = ["all-all"]
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_name" {
+  description = "The name of the VPC to reference."
+  type        = string
+  default     = "hanna-vpc" # Defaulting to hanna-vpc as requested
+}
