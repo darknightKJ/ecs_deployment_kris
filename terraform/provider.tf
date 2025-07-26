ab7394390f60843c17097a3759feca809925c968
darknightKJ <krisbianto@gmail.com>
2025-07-26 12:15:28 +0800

    Reinitializing Terraform project

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_name" {
   description = "The name of the VPC to reference."
   type        = string
-  default     = "kael-vpc" # Defaulting to kael-vpc as requested
+  default     = "hanna-vpc" #"kael-vpc"  Defaulting to kael-vpc as requested
 }
